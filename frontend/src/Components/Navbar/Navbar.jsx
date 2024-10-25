7d65d2e1bdfd1388fb0f8d5f664e9f62361bb0f9
Mohammed Parves <mdperrfan@gmail.com>
2024-10-25 15:20:01 +0600

    updated

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from '../../context/AppContext';
 const Navbar = () => {
     const navigate = useNavigate();
     const [showMenu, setShowMenu] = useState(false);
-    const {token,setToken}=useContext(AppContext);
+    const {token,setToken,userData}=useContext(AppContext);
 
     const logOut=()=>{
         setToken(false)
@@ -34,8 +34,8 @@ const Navbar = () => {
                     </NavLink>
                 </ul>
                 <div className='flex items-center gap-4'>
-                    {token ? <div className='flex items-center gap-3 cursor-pointer group relative'>
-                        <img className='w-8 rounded-full' src={assets.profile_pic} alt="img" />
+                    {token && userData? <div className='flex items-center gap-3 cursor-pointer group relative'>
+                        <img className='w-8 rounded-full' src={userData.image} alt="img" />
                         <img className='w-2.5' src={assets.dropdown_icon} alt="img" />
                         <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                             <div className='min-w-48 bg-stone-100 p-4 rounded flex flex-col gap-4'>
